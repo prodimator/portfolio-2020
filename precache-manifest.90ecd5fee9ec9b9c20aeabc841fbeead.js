1713242a70aa2b2c0d4dc111d32790c3f679f4e4
your_github_username <rwolfe22@gmail.com>
2020-08-27 08:50:52 -0700

    Updates

---
+++ precache-manifest.90ecd5fee9ec9b9c20aeabc841fbeead.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c62c09cfa2a01cd183e0de680997b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "797115c6443160cfc38b",
+    "url": "/static/css/2.ffb5e55f.chunk.css"
+  },
+  {
+    "revision": "32e3f1753e272ec33072",
+    "url": "/static/css/main.3da11a12.chunk.css"
+  },
+  {
+    "revision": "797115c6443160cfc38b",
+    "url": "/static/js/2.eb83a624.chunk.js"
+  },
+  {
+    "revision": "cce5145833db6848590a8d5f4b075fc6",
+    "url": "/static/js/2.eb83a624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e3f1753e272ec33072",
+    "url": "/static/js/main.409bdcb4.chunk.js"
+  },
+  {
+    "revision": "55cf03ad92c385910fbc",
+    "url": "/static/js/runtime-main.e40405f4.js"
+  },
+  {
+    "revision": "24cf4bfe7e74ad0f8dc02274f7ce2d0c",
+    "url": "/static/media/BugFix.24cf4bfe.svg"
+  },
+  {
+    "revision": "53a7aef523b95330d5c2c1cfb2a3e070",
+    "url": "/static/media/background3.53a7aef5.jpg"
+  },
+  {
+    "revision": "1389182360b79a642e8c7e22978cb8b0",
+    "url": "/static/media/meimage.13891823.jpg"
+  }
+]);
